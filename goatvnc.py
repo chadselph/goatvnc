1a38bb27303d678c33ac3de79f0a13850d163f2e
Chad Selph <chad.selph@gmail.com>
2011-08-01 18:22:58 -0700

    cosmetic changes

--- goatvnc.py
+++ goatvnc.py
@@ -8,9 +8,9 @@ from time import sleep
 from optparse import OptionParser
 
 class BaseVNCServer(Thread):
-    def __init__(self,conn,fbwidth=800,fbheight=600,name="goatvnc",image="gentoo.jpg"):
+    def __init__(self, conn, fbwidth=800, fbheight=600, name="goatvnc", image="gentoo.jpg"):
         self.conn = conn
-        self.options = {'fbwidth':fbwidth,'fbheight':fbheight,'name':name,'image':image}
+        self.options = {'fbwidth':fbwidth, 'fbheight':fbheight, 'name':name, 'image':image}
         Thread.__init__(self)
 
     def run(self):
@@ -19,34 +19,34 @@ class BaseVNCServer(Thread):
         self.handle_requests()
         self.conn.close()
 
-    def handshake(self,proto=(3,8)):
+    def handshake(self, proto=(3,8)):
         """
         Step 1: Determining the version 
         """
         self.conn.send("RFB %03d.%03d\n" % proto)
         version_response = self.conn.recv(1024)
-        m = re.search('RFB (\d\d\d)\.(\d\d\d)',version_response)
+        m = re.search('RFB (\d\d\d)\.(\d\d\d)', version_response)
         if m:
             protos =  m.groups()
             """
             self.proto is a tuple of (major,minor) protocol version number
             """
-            self.proto = (int(protos[0]),int(protos[1]))
+            self.proto = (int(protos[0]), int(protos[1]))
         else:
             return
         """
         Step 2: Determining the security type
         """
-        if self.proto in ((3,7),(3,8)):
-            sec_types = struct.pack('!BB',1,1)
+        if self.proto in ((3, 7), (3, 8)):
+            sec_types = struct.pack('!BB', 1, 1)
         elif self.proto in ((3,3)):
-            sec_types = struct.pack('i',1)
+            sec_types = struct.pack('i', 1)
         self.conn.send(sec_types)
         sec_response = self.conn.recv(1024)
         self.sec_method =  struct.unpack('!B',sec_response)[0]
         if self.sec_method:
-            self.conn.send(struct.pack('!i',0))
-        elif self.proto in ((3,8)):
+            self.conn.send(struct.pack('!i', 0))
+        elif self.proto in ((3, 8)):
             print self.conn.recv()
             return
         else:
@@ -60,6 +60,7 @@ class BaseVNCServer(Thread):
        server_init = struct.pack('!HH',fbwidth,fbheight) + pixel_format + struct.pack('!I',len(name))+name
        self.conn.send(server_init)
        self.imgdata = Image.open(image).resize((800,600)).convert('RGBA').tostring()
+
     def handle_requests(self):
         self.first_fbupdate = True
         msg = ""
@@ -75,10 +76,13 @@ class BaseVNCServer(Thread):
                     print "unknown request, noob"+type
             else:
                 self.conn.close()
+
     def do_SetPixel(self,packet):
         pass
+
     def do_SetEncodings(self,packet):
         pass
+
     def do_FramebufferUpdateRequest(self,packet):
         #print struct.unpack('!BBHHHH',packet)
         print "sending shock site"
@@ -90,36 +94,40 @@ class BaseVNCServer(Thread):
 
     def do_KeyEvent(self,packet):
         pass
+
     def do_PointerEvent(self,packet):
         print "pointer event!"
+
     def do_ClientCutText(self,packet):
         pass
+
     def next_request(self,packet):
         type = ord(packet[0])
         if type == 2:
-            length = struct.unpack('!xxH',packet[:4])[0]
+            length = struct.unpack('!xxH', packet[:4])[0]
         elif type == 6:
-            length = struct.unpack('!4xI',packet[:8])[0]
+            length = struct.unpack('!4xI', packet[:8])[0]
         else:
             length = 0
-        length = [20,0,4+4*length,10,8,6,8+length]
+        length = [20, 0, 4+4*length, 10, 8, 6, 8+length]
         return packet[length[type]:]
+
+
 class VNCDispatcher(object):
-    def __init__(self,host='',port=5900,handler=BaseVNCServer,args={}):
+    def __init__(self, host='', port=5900, handler=BaseVNCServer, args=None):
+        args = args or {}
         self.s =  socket(AF_INET,SOCK_STREAM)
-        self.s.bind((host,port))
+        self.s.bind((host, port))
         self.s.listen(1)
         while 1:
             conn, addr = self.s.accept()
             print 'Connection from: ',addr
-            h = handler(conn=conn,**args)
+            h = handler(conn=conn, **args)
             h.start()
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-p","--port",dest="port",type="int",help="TCP port to listen on")
-    parser.add_option("-i","--image",dest="image",help="Image to serve")
-    (options,args) = parser.parse_args()
-    port = options.port or 5900
-    image = options.image or "gentoo.jpg"
-    b = VNCDispatcher(port=port,args={'image':image})
+    parser.add_option("-p", "--port", dest="port", type="int", help="TCP port to listen on", default=5900)
+    parser.add_option("-i", "--image",dest="image", help="Image to serve", default="gentoo.jpg")
+    (options, args) = parser.parse_args()
+    b = VNCDispatcher(port=options.port, args={'image':options.image})
