1610957be664d3260ed64dd2acf4a19335ca3b28
Chad Selph <chad.selph@gmail.com>
2015-04-30 17:06:21 -0700

    some found code

--- src/goatvnc.py
+++ src/goatvnc.py
@@ -1,65 +1,69 @@
-from socket import *
-import sys
 import Image
 import struct
-import re
-from threading import Thread
 from time import sleep
-from optparse import OptionParser
 
-class BaseVNCServer(Thread):
-    def __init__(self,conn,fbwidth=800,fbheight=600,name="goatvnc",image="gentoo.jpg"):
-        self.conn = conn
-        self.options = {'fbwidth':fbwidth,'fbheight':fbheight,'name':name,'image':image}
-        Thread.__init__(self)
 
-    def run(self):
-        self.handshake()
-        self.initialize(**self.options)
-        self.handle_requests()
-        self.conn.close()
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol, Factory
 
-    def handshake(self,proto=(3,8)):
+
+class RFBServer(Protocol):
+    "Protocol for Remote Frame Buffer"
+    def __init__(self, fbwidth=800, fbheight=600, name="goatvnc", version=(3, 8)):
+
+        self.step = self.handshake
+        self.buff = []
+        self.options = {
+            'fbwidth': fbwidth,
+            'version': version,
+        }
+
+    def connectionMade(self):
+        self.transport.write("RFB 003.008\n")
+
+    def dataReceived(self, data):
+        print data
+        self.step(data)
+
+    def handshake(self, data):
         """
         Step 1: Determining the version 
         """
-        self.conn.send("RFB %03d.%03d\n" % proto)
-        version_response = self.conn.recv(1024)
-        m = re.search('RFB (\d\d\d)\.(\d\d\d)',version_response)
-        if m:
-            protos =  m.groups()
-            """
-            self.proto is a tuple of (major,minor) protocol version number
-            """
-            self.proto = (int(protos[0]),int(protos[1]))
+        if data[:3] == "RFB":
+            major,minor = [int(x) for x in data[4:].split('.')]
+            self.version = (major, minor)
         else:
             return
         """
         Step 2: Determining the security type
         """
-        if self.proto in ((3,7),(3,8)):
+        if self.version in ((3,7),(3,8)):
             sec_types = struct.pack('!BB',1,1)
-        elif self.proto in ((3,3)):
+        elif self.version in ((3,3)):
             sec_types = struct.pack('i',1)
-        self.conn.send(sec_types)
-        sec_response = self.conn.recv(1024)
+        self.transport.write(sec_types)
+        self.step = self.set_security
+
+
+    def set_security(self, data):
+        sec_response = data
         self.sec_method =  struct.unpack('!B',sec_response)[0]
         if self.sec_method:
-            self.conn.send(struct.pack('!i',0))
-        elif self.proto in ((3,8)):
-            print self.conn.recv()
+            self.transport.write(struct.pack('!i',0))
+        elif self.version in ((3,8)):
             return
         else:
             return
-        print "Connected success."
+        self.step = self.initialize
 
-    def initialize(self,fbwidth,fbheight,name,image):
-       shared = self.conn.recv(1024)
-       #I don't know how to do this part
-       pixel_format = struct.pack('!BBBBHHHBBBxxx',32,24,0,1,255,255,255,0,8,16)
-       server_init = struct.pack('!HH',fbwidth,fbheight) + pixel_format + struct.pack('!I',len(name))+name
-       self.conn.send(server_init)
-       self.imgdata = Image.open(image).resize((800,600)).convert('RGBA').tostring()
+
+    def initialize(self, data):
+        #I don't know how to do this part
+        pixel_format = struct.pack('!BBBBHHHBBBxxx',32,24,0,1,255,255,255,0,8,16)
+        server_init = struct.pack('!HH',self.options['fbwidth'],self.options['fbheight']) + pixel_format + struct.pack('!I',len(name))+name
+        self.conn.send(server_init)
+        self.imgdata = Image.open(image).resize((800,600)).convert('RGBA').tostring()
+
     def handle_requests(self):
         self.first_fbupdate = True
         msg = ""
@@ -75,10 +79,13 @@ class BaseVNCServer(Thread):
                     print "unknown request, noob"+type
             else:
                 self.conn.close()
+
     def do_SetPixel(self,packet):
         pass
+
     def do_SetEncodings(self,packet):
         pass
+
     def do_FramebufferUpdateRequest(self,packet):
         #print struct.unpack('!BBHHHH',packet)
         print "sending shock site"
@@ -90,10 +97,13 @@ class BaseVNCServer(Thread):
 
     def do_KeyEvent(self,packet):
         pass
+
     def do_PointerEvent(self,packet):
         print "pointer event!"
+
     def do_ClientCutText(self,packet):
         pass
+
     def next_request(self,packet):
         type = ord(packet[0])
         if type == 2:
@@ -104,6 +114,8 @@ class BaseVNCServer(Thread):
             length = 0
         length = [20,0,4+4*length,10,8,6,8+length]
         return packet[length[type]:]
+
+"""
 class VNCDispatcher(object):
     def __init__(self,host='',port=5900,handler=BaseVNCServer,args={}):
         self.s =  socket(AF_INET,SOCK_STREAM)
@@ -114,8 +126,10 @@ class VNCDispatcher(object):
             print 'Connection from: ',addr
             h = handler(conn=conn,**args)
             h.start()
+            """
 
 if __name__ == "__main__":
+    """
     parser = OptionParser()
     parser.add_option("-p","--port",dest="port",type="int",help="TCP port to listen on")
     parser.add_option("-i","--image",dest="image",help="Image to serve")
@@ -123,3 +137,9 @@ if __name__ == "__main__":
     port = options.port or 5900
     image = options.image or "gentoo.jpg"
     b = VNCDispatcher(port=port,args={'image':image})
+    """
+
+    factory = Factory()
+    factory.protocol = RFBServer
+    reactor.listenTCP(5900, factory)
+    reactor.run()
